4af24fba2fa92cb2ea700c94a6ca92e7ff508fd0
arman_hossain <md.armantext@gmail.com>
2024-08-28 23:29:14 +0600

    add a array a function

---
+++ class/index.php
@@ -0,0 +1,41 @@
+<?php
+
+$name = "Foysal";
+
+$user = [
+    "firstName" => "Mohammad",
+    "lastName" => "Arman",
+    "Sadiur Rahaman Setu",
+    7 => "Manikganj",
+    "CTG",
+    4 => "Dhaka",
+    "Comiila",
+    "name" => "$name",
+    7 => "Pabna",
+    6 => "Pabna",
+    1 => "Arafat", // 1
+    "1" => "Hoosain", // 1
+    true => "Arman", // 1
+
+];
+
+$number = [10, 20, 30, 50, 60];
+
+// array_push($number, 80); add element in last on the array
+// array_push($number, 800);
+// array_pop($number);  delet the last element on the array
+// array_unshift($number, 100); add a element fist on the array
+// array_shift($number); remove the element fist on the array
+// rsort($number);
+// sort($number);
+// $number = array_sum($number);
+
+
+
+
+echo '<pre>';
+// print_r($user);
+// var_dump(count($number));
+var_dump($number);
+echo '</pre>';
+
