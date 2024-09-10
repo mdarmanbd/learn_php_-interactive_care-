9cfd4ee8d5d85f6bca9b82fc1dac0eea236b0a05
arman_hossain <md.armantext@gmail.com>
2024-09-10 23:52:13 +0600

    add the problem php

---
+++ class/problem.php
@@ -0,0 +1,283 @@
+<?php
+//✅Coding challenge #1. Coding challenge #1: Print numbers from 1 to 10
+
+// $number = [];
+
+// for($i = 1; $i <= 10; $i++){
+//     $number [] = $i;
+// }
+// echo "<pre>";
+// print_r($number);
+// echo "</pre>";
+
+//✅Coding challenge #2. Coding challenge #2: Print the odd numbers less than 100
+
+// $oddNumber = [];
+
+// for($i = 1; $i <=100; $i++){
+//     if($i % 2 == 0){
+//         $oddNumber[] = $i;
+//     }
+// }
+// echo "<pre>";
+// print_r($oddNumber);
+// echo "</pre>";
+
+// ✅Coding challenge #3: Print the multiplication table with 7
+
+// for($i = 1; $i <= 10; $i++){
+//    $result =  7 * $i ;
+//    echo "<pre>";
+//    echo "7 * " . $i . " = " . $result;
+//    echo "</pre>";
+// }
+
+
+//✅ Coding challenge #4: Print all the multiplication tables with numbers from 1 to 10
+
+// for ($i = 1; $i <=10; $i++){
+//     if($i == 1){
+//         for($x = 1; $x <= 10; $x++){
+//             $result = 1 * $x;
+//             echo "<pre>";
+//             echo "1 * " . $x . " = " . $result;
+//             echo "</pre>";
+//         }
+//     }
+
+//     echo "<br>";
+
+//     if($i == 2){
+//         for($x = 1; $x <= 10; $x++){
+//             $result = 2 * $x;
+//             echo "<pre>";
+//             echo "2 * " . $x . " = " . $result;
+//             echo "</pre>";
+//         }
+//     }
+
+//     echo "<br>";
+
+//     if($i == 3){
+//         for($x = 1; $x <= 10; $x++){
+//             $result = 3 * $x;
+//             echo "<pre>";
+//             echo "3 * " . $x . " = " . $result;
+//             echo "</pre>";
+//         }
+//     }
+
+//     echo "<br>";
+
+//     if($i == 4){
+//         for($x = 1; $x <= 10; $x++){
+//             $result = 4 * $x;
+//             echo "<pre>";
+//             echo "4 * " . $x . " = " . $result;
+//             echo "</pre>";
+//         }
+//     }
+
+//     echo "<br>";
+
+//     if($i == 5){
+//         for($x = 1; $x <= 10; $x++){
+//             $result = 5 * $x;
+//             echo "<pre>";
+//             echo "5 * " . $x . " = " . $result;
+//             echo "</pre>";
+//         }
+//     }
+
+// }
+
+
+// ✅Coding challenge #5: Calculate the sum of numbers from 1 to 10
+
+// $number = [];
+
+// for($i = 1; $i <= 10; $i++){
+//     $number[] = $i;
+// }
+// print_r($number) ;
+
+// $number_sum = array_sum($number);
+// echo $number_sum;
+
+
+
+// ✅Coding challenge #6: Calculate 10!
+
+
+//✅ Coding challenge #7: Calculate the sum of odd numbers greater than 10 and less than 30
+
+// $number = [];
+
+// for($i = 1; $i <= 30; $i++){
+//     if($i % 2 == 0){
+//         if( $i > 10 && $i < 30){
+//             $number [] = $i;
+//         }
+//     }
+// }
+
+// echo "<pre>";
+// print_r(array_sum($number));
+// echo "</pre>";
+
+// ✅Coding challenge #8: Create a function that will convert from Celsius to Fahrenheit
+
+// $temp = 30;
+
+// function celsius_fahrenheit($tem){
+//     $celsius = $tem*9/5+32;
+//     return($celsius);
+// }
+// echo (int) celsius_fahrenheit($temp) . "deg" . " fer";
+
+// echo "<br>";
+
+
+//✅ Coding challenge #9: Create a function that will convert from Fahrenheit to Celsius
+
+
+// function fahrenheit_celsius($tem){
+//     return(5/9*($tem-32));
+// }
+// echo (int) fahrenheit_celsius(50). "deg" . " cel";
+
+// ✅Coding challenge #10: Calculate the sum of numbers in an array of numbers
+
+
+
+//✅ Coding challenge #11: Calculate the average of the numbers in an array of numbers
+
+
+
+// ✅Coding challenge #12: Create a function that receives an array of numbers and returns an array containing only the positive numbers
+
+
+//✅ Coding challenge #13: Print the first 10 Fibonacci numbers without recursion
+
+
+// ✅Coding challenge #14: Create a function that will find the nth Fibonacci number using recursion
+
+
+// ✅Coding challenge #15: Create a function that will return a Boolean specifying if a number is prime
+
+
+// ✅Coding challenge #16: Calculate the sum of digits of a positive integer number
+
+
+
+// ✅Coding challenge #17: Print the first 100 prime numbers
+
+
+// ✅Coding challenge #18: Create a function that will return in an array the first "nPrimes" prime numbers greater than a particular number "startAt"
+
+
+// ✅Coding challenge #19: Rotate an array to the left 1 position
+
+
+//✅ Coding challenge #20: Create a function that will merge two arrays and return the result as a new array
+
+// ✅Coding challenge #21: Create a function that will receive two arrays of numbers as arguments and return an array composed of all the numbers that are either in the first array or second array but not in both
+
+
+// ✅Coding challenge #22: Create a function that will receive two arrays and will return an array with elements that are in the first array but not in the second
+
+
+//✅ Coding challenge #23: Create a function that will receive an array of numbers as argument and will return a new array with distinct elements
+
+
+
+// ✅ Coding challenge #24: Calculate the sum of first 100 prime numbers
+
+
+// ✅Coding challenge #25: Print the distance between the first 100 prime numbers
+
+
+
+
+// ✅Coding challenge #26: Create a function that will add two positive numbers of indefinite size. The numbers are received as strings and the result should be also provided as string.
+
+
+
+// ✅Coding challenge #27. Create a function that will return the number of words in a text
+
+
+// ✅Coding challenge #28. Create a function that will capitalize the first letter of each word in a text
+
+
+
+// ✅Coding challenge #29. Calculate the sum of numbers received in a comma delimited string
+
+
+// ✅Coding challenge #30. Create a function that will return an array with words inside a text
+
+
+// ✅Coding challenge #31. Create a function to convert a CSV text to a “bi-dimensional” array
+
+
+
+// ✅Coding challenge #32. Create a function that converts a string to an array of characters
+
+
+// ✅Coding challenge #33. Create a function that will convert a string in an array containing the ASCII codes of each character
+
+
+
+//✅ Coding challenge #34. Create a function that will convert an array containing ASCII codes in a string
+
+
+// ✅Coding challenge #35. Implement the Caesar cypher
+
+
+
+// ✅Coding challenge #36. Implement the bubble sort algorithm for an array of numbers
+
+
+
+// ✅Coding challenge #37. Create a function to calculate the distance between two points defined by their x, y coordinates
+
+
+
+// ✅Coding challenge #38. Create a function that will return a Boolean value indicating if two circles defined by center coordinates and radius are intersecting
+
+
+
+//✅ Coding challenge 39. Create a function that will receive a bi-dimensional array as argument and a number and will extract as a unidimensional array the column specified by the number
+
+
+
+// ✅Coding challenge #40. Create a function that will convert a string containing a binary number into a number
+
+
+// ✅Coding challenge #41. Create a function to calculate the sum of all the numbers in a jagged array (array contains numbers or other arrays of numbers on an unlimited number of levels)
+
+
+
+// ✅Coding challenge #42. Find the maximum number in a jagged array of numbers or array of numbers
+
+
+// ✅Coding challenge #43. Deep copy a jagged array with numbers or other arrays in a new array
+
+
+// ✅Coding challenge #44. Create a function to return the longest word(s) in a string
+
+
+
+// ✅Coding challenge #45. Create a function that will receive n as argument and return an array of n unique random numbers from 1 to n.
+
+
+
+
+// ✅Coding challenge #46. Find the frequency of characters inside a string. Return the result as an array of objects. Each object has 2 fields: character and number of occurrences.
+
+
+//✅ Coding challenge #47. Calculate Fibonacci(500) with high precision (all decimals)
+
+
+
+
+// ✅Coding challenge #48. Calculate 70! with high precision (all digits)
