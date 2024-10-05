e9db2e62ec0509cd0b1da2e53f08c9943c9f5d84
arman_hossain <md.armantext@gmail.com>
2024-10-06 00:47:58 +0600

    learn about extended

--- class/oop/extended.php
+++ class/oop/extended.php
@@ -1,5 +1,35 @@
 <?php 
 
-echo " Extended property";
+class parentClass{
+    protected $name;
+
+    function __construct($newName = ''){
+        $this-> name = $newName;
+        $this->sayHi();
+        $this->sayBro();
+    }
+
+    // function sayHi(){
+    //     echo "Hi from {$this->name} <br>";
+    // }
+
+    function sayHello(){
+        echo "My name is {$this->name} <br>";
+    }
+
+    function sayBro(){
+        echo " Hi bro <br>";
+    }
+}
+
+class childClass extends parentClass{
+    function sayHi(){
+        parent::sayHello();
+        // echo "Hello world 123 <br>";
+    }
+}
+
+$childClass = new childClass("Abu ubaida");
+
 
 ?>
